d2badb2d07042ff44dd8b28438555a3e2082f312
kpu-lotim <majikongo@gmail.com>
2026-06-24 14:02:23 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'pwa-app-v1';
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(['/', '/index.html'])));
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => res || fetch(event.request))
+    );
+});
